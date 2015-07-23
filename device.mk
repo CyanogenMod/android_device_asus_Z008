3c8e34aa338032226a4449d598b55e7a1ec7a730
Yakub <yakub234go@gmail.com>
2015-07-23 16:33:50 +0530

    changes to device.mk for Z008
    Change-Id: I49d838c0bc0bde65981d9f5e1a4460e768f38e65

--- device.mk
+++ device.mk
@@ -14,20 +14,20 @@
 # limitations under the License.
 #
 
-PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi xxhdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=320
 
 # call dalvik heap config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # call hwui memory config
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+#$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 DEVICE_PACKAGE_OVERLAYS := \
-    device/asus/Z00A/overlay
+    device/asus/Z008/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
@@ -77,8 +77,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/configs/gps.conf:system/etc/gps.conf \
-    device/asus/Z00A/configs/gps.xml:system/etc/gps.xml
+    device/asus/Z008/configs/gps.conf:system/etc/gps.conf \
+    device/asus/Z008/configs/gps.xml:system/etc/gps.xml
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.spid.gps.tty=ttyMFD2
@@ -96,7 +96,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # specific management of audio_policy.conf
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/configs/audio_policy.conf:system/etc/audio_policy.conf
+    device/asus/Z008/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # Add props used in stock
 PRODUCT_PROPERTY_OVERRIDES +=  \
@@ -118,8 +118,8 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.isUsbOtgEnabled=1
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
 
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
-    device/asus/Z00A/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal 
+    device/asus/Z008/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
+    device/asus/Z008/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal 
 
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.hfp.WideBandSpeechEnabled=true
@@ -130,13 +130,13 @@ PRODUCT_PACKAGES += \
 
 #Video
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/asus/Z00A/media/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
+    device/asus/Z008/media/media_profiles.xml:system/etc/media_profiles.xml \
+    device/asus/Z008/media/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/asus/Z00A/media/media_codecs.xml:system/etc/media_codecs.xml \
-    device/asus/Z00A/media/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf \
-    device/asus/Z00A/media/video_isv_profile.xml:system/etc/video_isv_profile.xml
+    device/asus/Z008/media/media_codecs.xml:system/etc/media_codecs.xml \
+    device/asus/Z008/media/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf \
+    device/asus/Z008/media/video_isv_profile.xml:system/etc/video_isv_profile.xml
 
 # psb video
 PRODUCT_PACKAGES += \
@@ -207,7 +207,7 @@ PRODUCT_PACKAGES += \
     bcmdhd.cal
 
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/wifi/wpa_supplicant.conf:/system/etc/wifi/wpa_supplicant.conf
+    device/asus/Z008/wifi/wpa_supplicant.conf:/system/etc/wifi/wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
@@ -237,13 +237,13 @@ PRODUCT_COPY_FILES += \
 
 # Key layout files
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/keylayout/ASUS_TransKeyboard.kl:system/usr/keylayout/ASUS_TransKeyboard.kl \
-    device/asus/Z00A/keylayout/ftxxxx_ts.kl:system/usr/keylayout/ftxxxx_ts.kl \
-    device/asus/Z00A/keylayout/ftxxxx_ts.kcm:system/usr/keychars/ftxxxx_ts.kcm \
-    device/asus/Z00A/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    device/asus/Z00A/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl:system/usr/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl \
-    device/asus/Z00A/keylayout/Vendor_0b05_Product_17fc.kl:system/usr/keylayout/Vendor_0b05_Product_17fc.kl \
-    device/asus/Z00A/keylayout/Vendor_0b05_Product_1803.kl:system/usr/keylayout/Vendor_0b05_Product_1803.kl
+    device/asus/Z008/keylayout/ASUS_TransKeyboard.kl:system/usr/keylayout/ASUS_TransKeyboard.kl \
+    device/asus/Z008/keylayout/ftxxxx_ts.kl:system/usr/keylayout/ftxxxx_ts.kl \
+    device/asus/Z008/keylayout/ftxxxx_ts.kcm:system/usr/keychars/ftxxxx_ts.kcm \
+    device/asus/Z008/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    device/asus/Z008/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl:system/usr/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl \
+    device/asus/Z008/keylayout/Vendor_0b05_Product_17fc.kl:system/usr/keylayout/Vendor_0b05_Product_17fc.kl \
+    device/asus/Z008/keylayout/Vendor_0b05_Product_1803.kl:system/usr/keylayout/Vendor_0b05_Product_1803.kl
 
 # Keyhandler
 PRODUCT_PACKAGES += \
@@ -251,12 +251,12 @@ PRODUCT_PACKAGES += \
 
 #GFX Config
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/powervr.ini:system/etc/powervr.ini
+    device/asus/Z008/powervr.ini:system/etc/powervr.ini
 
 # NFC
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/asus/Z00A/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf
+    device/asus/Z008/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/asus/Z008/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf
 
 # NFC packages
 PRODUCT_PACKAGES += \
@@ -273,16 +273,16 @@ ENABLE_ITUXD := true
 PRODUCT_PACKAGES += \
     ituxd
 
-$(call inherit-product-if-exists, vendor/asus/Z00A/Z00A-vendor.mk)
-$(call inherit-product-if-exists, vendor/intel/PRIVATE/fhd/device-vendor.mk)
-$(call inherit-product-if-exists, vendor/intel/Z00Abuilts/houdini/houdini.mk)
+$(call inherit-product-if-exists, vendor/asus/Z008/Z008-vendor.mk)
+$(call inherit-product-if-exists, vendor/intel/PRIVATE/hd/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/intel/Z008builts/houdini/houdini.mk)
 
 # Add WiFi Firmware
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4339/device-bcm.mk)
 
 # specific management of sep_policy.conf
 PRODUCT_COPY_FILES += \
-    device/asus/Z00A/sep_policy.conf:system/etc/security/sep_policy.conf
+    device/asus/Z008/sep_policy.conf:system/etc/security/sep_policy.conf
 
 # hardware optimizations
 #PRODUCT_PROPERTY_OVERRIDES += \
