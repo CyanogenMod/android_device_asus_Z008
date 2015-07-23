9a6d8b7d7eb13842ab2db7576c655bf199116b78
Yakub <yakub234go@gmail.com>
2015-07-23 16:21:29 +0530

    changes to cm.mk for Z008
    Change-Id: I90398b7ade381b0622244b02be9a16534943d507

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1080
-TARGET_SCREEN_WIDTH := 1920
+TARGET_SCREEN_HEIGHT := 720
+TARGET_SCREEN_WIDTH := 1280
 
 # Inherit some common CM stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -8,23 +8,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/asus/Z00A/device.mk)
+$(call inherit-product, device/asus/Z008/device.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
+DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_Z00A
+PRODUCT_NAME := cm_Z008
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_Z00A
+PRODUCT_MODEL := ASUS_Z008
 PRODUCT_MANUFACTURER := asus
-PRODUCT_DEVICE := Z00A
+PRODUCT_DEVICE := Z008
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=WW_Z00A \
-    BUILD_FINGERPRINT=asus/WW_Z00A/Z00A:5.0/LRX21V/2.19.40.22_20150627_5104_user:user/release-keys \
-    PRIVATE_BUILD_DESC="asusmofd_fhd-user 5.0 LRX21V 2.19.40.22_20150627_5104_user release-keys"
+    PRODUCT_NAME=WW_Z008 \
+    BUILD_FINGERPRINT=asus/WW_Z008/Z008:5.0/LRX21V/2.19.40.23_20150701_1487_user:user/release-keys \
+    PRIVATE_BUILD_DESC="asusmofd_hd-user 5.0 LRX21V 2.19.40.23_20150701_1487_user release-keys"
